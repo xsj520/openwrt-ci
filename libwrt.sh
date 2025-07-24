63b63eb33d3d242bd6c2aa69ce6cf821dcc43524
xsj520 <72717602+xsj520@users.noreply.github.com>
2025-07-24 08:38:11 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,6 +1,7 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
