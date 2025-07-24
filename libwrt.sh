db0753f068d1b34918702c653d91d08b2387696e
xsj520 <72717602+xsj520@users.noreply.github.com>
2025-07-24 17:23:29 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,5 +1,3 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-
